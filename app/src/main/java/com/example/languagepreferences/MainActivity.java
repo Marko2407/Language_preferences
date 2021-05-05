f03491603009220a084cbd4ab00ae6efc683a158
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-06 01:44:04 +0200

    adding menu for changing language

--- app/src/main/java/com/example/languagepreferences/MainActivity.java
+++ app/src/main/java/com/example/languagepreferences/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.languagepreferences;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,15 +8,42 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     TextView textView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.croatian:
+                setLanguage("Croatian");
+                return true;
+            case R.id.english:
+                setLanguage("English");
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
     public void setLanguage(String language){
         sharedPreferences.edit().putString("language",language).apply();
         textView.setText(language);
-
     }
 
     @Override
